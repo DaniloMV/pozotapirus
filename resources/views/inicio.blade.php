ceda32d98fb39fa5d26094c17962549abf558597
Danilo <danilo7valladarez@gmail.com>
2015-03-04 20:49:22 -0500

    b

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,11 +3,12 @@
 
 @section('sidebar')
     @parent
- <h1>Página de Bienvenida</h1>
     <!-- <p>This is appended to the master sidebar.</p> -->
 @stop
 <!-- Lo que debe contener en el body -->
 @section('content')
     <!-- ponemos el contenido de la vista estamos dentro del body -->
+
+    <input>
 
 @stop
